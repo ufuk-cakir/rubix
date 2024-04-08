bb80a1fae6f6d0acbade03e19efee2d9cd7d6146
Ufuk Çakır <ufukcakir2001@gmail.com>
2024-04-08 21:38:47 +0200

    fix illustris api test creating files

--- rubix/galaxy/_input_handler/_illustris_api.py
+++ rubix/galaxy/_input_handler/_illustris_api.py
@@ -1,22 +1,23 @@
-import requests
 import os
+
 import h5py
+import requests
 
 
 class IllustrisAPI:
-    '''This class is used to load data from the Illustris API.
-    
+    """This class is used to load data from the Illustris API.
+
     It loads both subhalo data and particle data from a given simulation, snapshot, and subhalo ID.
     The default fields that are downloaded are:
     - For gas particles: "Coordinates", "Density", "Masses", "ParticleIDs", "GFM_Metallicity", "SubfindHsml",
     "StarFormationRate", "InternalEnergy", "Velocities", "ElectronAbundance", "GFM_Metals"
-    
+
     - For star particles: "Coordinates", "GFM_InitialMass", "Masses", "ParticleIDs", "GFM_Metallicity",
     "GFM_StellarFormationTime", "Velocities"
-    
+
     Check the source for the API documentation for more information: https://www.tng-project.org/data/docs/api/
-    '''
-    DATAPATH = "./tempdata"
+    """
+
     URL = "http://www.tng-project.org/api/"
     DEFAULT_FIELDS = {
         "PartType0": [
@@ -49,6 +50,7 @@ class IllustrisAPI:
         particle_type="stars",
         simulation="TNG50-1",
         snapshot=99,
+        save_data_path="./tempdata",
     ):
         """Illustris API class.
 
@@ -74,7 +76,7 @@ class IllustrisAPI:
         self.snapshot = snapshot
         self.simulation = simulation
         self.baseURL = f"{self.URL}{self.simulation}/snapshots/{self.snapshot}"
-
+        self.DATAPATH = save_data_path
 
     def _get(self, path, params=None, name=None):
         """Get data from the Illustris API.
@@ -101,7 +103,7 @@ class IllustrisAPI:
             r.raise_for_status()
         except requests.exceptions.HTTPError as err:
             raise ValueError(err)
-        
+
         if r.headers["content-type"] == "application/json":
             return r.json()  # parse json responses automatically
         if "content-disposition" in r.headers:
@@ -157,7 +159,7 @@ class IllustrisAPI:
         file_path = os.path.join(self.DATAPATH, f"{filename}.hdf5")
         if not os.path.exists(file_path):
             raise ValueError(f"File {file_path} does not exist.")
-        
+
         with h5py.File(file_path, "r") as f:
             for type in f.keys():
                 if type == "Header":
@@ -169,7 +171,7 @@ class IllustrisAPI:
 
         return returndict
 
-    def get_particle_data(self, id:int, particle_type, fields:str=DEFAULT_FIELDS):
+    def get_particle_data(self, id: int, particle_type, fields: str = DEFAULT_FIELDS):
         """Get particle data from the Illustris API.
 
         Returns the particle data for the given subhalo ID.
@@ -202,29 +204,29 @@ class IllustrisAPI:
         data = self._load_hdf5("cutout")
         return data
 
-    def load_galaxy(self, id:int , verbose=False):
-        '''Download Galaxy Data from the Illustris API.
-        
+    def load_galaxy(self, id: int, verbose=False):
+        """Download Galaxy Data from the Illustris API.
+
         This function downloads both the subhalo data and the particle data for stars and gas particles, for the fields specified in DEFAULT_FIELDS.
         It saves the data in a HDF5 file.
-        
+
         Parameters
         ----------
         id : int
             The ID of the subhalo to download.
         verbose : bool
             Whether to print out information about the download.
-            
+
         Returns
         -------
         dict
             The galaxy data.
-        
+
         Examples
         --------
         >>> illustris_api = IllustrisAPI(api_key, simulation="TNG50-1", snapshot=99)
         >>> data = illustris_api.load_galaxy(id=0, verbose=True)
-        '''
+        """
         if verbose:
             print(f"Getting data for subhalo {id}")
 
@@ -239,12 +241,13 @@ class IllustrisAPI:
         self._append_subhalo_data(subhalo_data, id)
         data = self._load_hdf5(filename=f"galaxy-id-{id}")
         return data
+
     def _append_subhalo_data(self, subhalo_data, id):
         # Append subhalo data to the HDF5 file
         file_path = os.path.join(self.DATAPATH, f"galaxy-id-{id}.hdf5")
         with h5py.File(file_path, "a") as f:
             f.create_group("SubhaloData")
             for key in subhalo_data.keys():
-                if type(subhalo_data[key]) == dict:
+                if isinstance(subhalo_data[key], dict):
                     continue
                 f["SubhaloData"].create_dataset(key, data=subhalo_data[key])
