838ac63f070f52c37ce778b6bc93ae17aa107e83
Ufuk Çakir <92611643+ufuk-cakir@users.noreply.github.com>
2024-05-02 23:17:08 +0200

    Telescope Configuration (#16)
    * first version of telescope factory
    
    * add ang size calculation
    
    * Merge commit '307943a371eda0c491c6666f7b85296d23cd78a1'
    
    * change basetelescope to eqx module
    
    * add tests for telescope class
    
    * ignore output in notebook
    
    * change type support for python 3.8
    
    * change dependencies
    
    * update jax version
    
    * update
    
    * make particle spaxel assignment more explicit
    
    * add the pixel type inside config
    
    * add pixel type in notebook demo

---
+++ rubix/telescope/base.py
@@ -0,0 +1,16 @@
+from jaxtyping import Float, Array
+import equinox as eqx
+
+
+class BaseTelescope(eqx.Module):
+    fov: float
+    spatial_res: float
+    wave_range: Float[Array, "2"]  # upper and lower limits
+    wave_res: float
+    lsf_fwhm: float
+    signal_to_noise: float
+    wave_centre: float
+    sbin: int
+    aperture_region: Float[Array, " sbin*sbin"]
+    pixel_type: str
+    name: str = "BaseTelescope"
