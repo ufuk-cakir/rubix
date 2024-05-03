528129c2813c32616f028404f5d9af01110aa1f8
Ufuk Çakır <ufukcakir2001@gmail.com>
2024-05-03 13:36:15 +0200

    convert arrays to jax units and refactor code

--- rubix/spectra/ssp/grid.py
+++ rubix/spectra/ssp/grid.py
@@ -1,8 +1,9 @@
 from jaxtyping import Float, Array
-from equinox import Module, field  # type: ignore #noqa
+import equinox as eqx
 from astropy import units as u
 import os
 import h5py
+import jax.numpy as jnp
 
 # TODO move this to the config file
 SSP_UNITS = {
@@ -13,16 +14,26 @@ SSP_UNITS = {
 }
 
 
-class SSPGrid(Module):
+class SSPGrid(eqx.Module):
     """
     Base class for all SSP
     """
 
-    # TODO: convert this into jax arrays
     age: Float[Array, " age_bins"]
     metallicity: Float[Array, " metallicity_bins"]
     wavelength: Float[Array, " wavelength_bins"]
-    flux: Float[Array, " age_bins metallicity_bins wavelength_bins"]
+    flux: Float[Array, "age_bins metallicity_bins wavelength_bins"]
+
+    def __init__(self, age, metallicity, wavelength, flux):
+        self.age = jnp.asarray(age)
+        self.metallicity = jnp.asarray(metallicity)
+        self.wavelength = jnp.asarray(wavelength)
+        self.flux = jnp.asarray(flux)
+
+    @staticmethod
+    def convert_units(data, from_units, to_units):
+        quantity = u.Quantity(data, from_units)
+        return quantity.to(to_units).value
 
     @classmethod
     def from_hdf5(cls, config: dict, file_location: str) -> "SSPGrid":
@@ -39,27 +50,19 @@ class SSPGrid(Module):
         SSPGrid
             The SSP grid in the correct units.
         """
-        if config["format"].lower() != "hdf5":
-            raise ValueError("The format of the file is not HDF5.")
+        if config.get("format", "").lower() != "hdf5":
+            raise ValueError("Configured file format is not HDF5.")
 
         file_path = os.path.join(file_location, config["file_name"])
-        fields_dict = config["fields"]
         ssp_data = {}
         with h5py.File(file_path, "r") as f:
-            for field_name, data in fields_dict.items():
-                field_name_in_file = data["name"]
-                field_units = data["units"]
-                field_log = data["in_log"]
-
-                if field_log:
-                    field_data = 10 ** f[field_name_in_file][:]  # type: ignore
-
-                # Get the data in the correct units using astropy
-                field_data = u.Quantity(f[field_name_in_file][:], field_units)  # type: ignore
-
-                # Convert to the correct units
-                field_data = field_data.to(SSP_UNITS[field_name])
-                ssp_data[field_name] = field_data
+            for field_name, field_info in config["fields"].items():
+                data = f[field_info["name"]][:]  # type: ignore
+                data = jnp.power(10, data) if field_info["in_log"] else data  # type: ignore
+                data = cls.convert_units(
+                    data, field_info["units"], SSP_UNITS[field_name]
+                )
+                ssp_data[field_name] = data
 
         grid = cls(**ssp_data)
         grid.__class__.__name__ = config["name"]
