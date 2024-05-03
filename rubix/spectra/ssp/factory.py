5c79de9fbe6bdad3ad522baae0fd6b4cf7c3b1c5
Ufuk Çakır <ufukcakir2001@gmail.com>
2024-05-03 11:18:34 +0200

    implement loading from template path

--- rubix/spectra/ssp/factory.py
+++ rubix/spectra/ssp/factory.py
@@ -1,34 +1,33 @@
 from rubix.utils import read_yaml
 from rubix.spectra.ssp.grid import SSPGrid
+from rubix import config as rubix_config
+from rubix.paths import TEMPLATE_PATH
 import os
 
 
-def get_ssp_template(name: str)-> SSPGrid:
+def get_ssp_template(name: str) -> SSPGrid:
     """
     Get the SSP template from the configuration file.
-    
+
     Returns
     -------
     SSPGrid
         The SSP template.
     """
-    PATH = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(PATH, "ssp_config.yml")
-    config = read_yaml(config_path) # TODO change this to load from rubix config
-    
-    config = config["templates"]
+
+    config = rubix_config["ssp"]["templates"]
     # Check if the template exists in config
     if name not in config:
-        raise ValueError(f"SSP template {name} not found in the supported configuration file.")
-    
-    
+        raise ValueError(
+            f"SSP template {name} not found in the supported configuration file."
+        )
+
     if config[name]["format"].lower() == "hdf5":
         try:
-            return SSPGrid.from_hdf5(config[name])
+            return SSPGrid.from_hdf5(config[name], file_location=TEMPLATE_PATH)
         except ValueError as e:
             raise ValueError(f"Error loading SSP template {name}: {e}")
-    
+
     else:
         raise ValueError("Currently only HDF5 format is supported for SSP templates.")
-        
-    
+
--- rubix/spectra/ssp/grid.py
+++ rubix/spectra/ssp/grid.py
@@ -1,6 +1,7 @@
 from jaxtyping import Float, Array
-from equinox import Module, field
+from equinox import Module, field  # type: ignore #noqa
 from astropy import units as u
+import os
 import h5py
 
 # TODO move this to the config file
@@ -8,30 +9,31 @@ SSP_UNITS = {
     "age": "Gyr",
     "metallicity": "",
     "wavelength": "Angstrom",
-    "flux": "Lsun/Angstrom"
+    "flux": "Lsun/Angstrom",
 }
+
 
 class SSPGrid(Module):
     """
     Base class for all SSP
     """
-    
+
+    # TODO: convert this into jax arrays
     age: Float[Array, " age_bins"]
     metallicity: Float[Array, " metallicity_bins"]
     wavelength: Float[Array, " wavelength_bins"]
     flux: Float[Array, " age_bins metallicity_bins wavelength_bins"]
-    
 
     @classmethod
-    def from_hdf5(cls, config: dict) -> "SSPGrid":
+    def from_hdf5(cls, config: dict, file_location: str) -> "SSPGrid":
         """
         Load a SSP grid from a HDF5 file.
-        
+
         Parameters
         ----------
         config : dict
             Configuration dictionary.
-            
+
         Returns
         -------
         SSPGrid
@@ -39,36 +41,26 @@ class SSPGrid(Module):
         """
         if config["format"].lower() != "hdf5":
             raise ValueError("The format of the file is not HDF5.")
-        
-        file_path = config["file_path"]
+
+        file_path = os.path.join(file_location, config["file_name"])
         fields_dict = config["fields"]
         ssp_data = {}
         with h5py.File(file_path, "r") as f:
-            for field, data in fields_dict.items():
+            for field_name, data in fields_dict.items():
                 field_name_in_file = data["name"]
                 field_units = data["units"]
                 field_log = data["in_log"]
-                
+
                 if field_log:
-                    field_data = 10**f[field_name_in_file][:]
-                
+                    field_data = 10 ** f[field_name_in_file][:]  # type: ignore
+
                 # Get the data in the correct units using astropy
-                field_data = u.Quantity(f[field_name_in_file][:], field_units)
-                
+                field_data = u.Quantity(f[field_name_in_file][:], field_units)  # type: ignore
+
                 # Convert to the correct units
-                field_data = field_data.to(SSP_UNITS[field])
-                ssp_data[field] = field_data
-        
+                field_data = field_data.to(SSP_UNITS[field_name])
+                ssp_data[field_name] = field_data
+
         grid = cls(**ssp_data)
         grid.__class__.__name__ = config["name"]
         return grid
-                
-        
-            
-            
-
-        
-        
-        
-        
-
