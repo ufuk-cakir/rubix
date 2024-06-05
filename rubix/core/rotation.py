a104c5bd668499ef24d12969287a4c5262a25493
Ufuk Çakır <ufukcakir2001@gmail.com>
2024-06-05 12:08:17 +0200

    implement first version of galaxy rotation into pipeline
    does not work currently, need to change the galaxy roation such that it works with jit

--- rubix/core/rotation.py
+++ rubix/core/rotation.py
@@ -1,15 +1,45 @@
 from typing import Dict
 import jax
+from rubix.logger import get_logger
+from rubix.galaxy.alignment import rotate_galaxy as rotate_galaxy_core
 
 
-# TODO: implement the real rotation function
-# this is currently only a placeholder
 def get_galaxy_rotation(config: dict):
+    # Check if rotation information is provided under galaxy config
+    if "rotation" not in config["galaxy"]:
+        raise ValueError("Rotation information not provided in galaxy config")
+    # Check if alpha, beta, gamma are provided
+    for key in ["alpha", "beta", "gamma"]:
+        if key not in config["galaxy"]["rotation"]:
+            raise ValueError(f"{key} not provided in rotation information")
+
+    # Get the rotation angles from the user config
+    alpha = config["galaxy"]["rotation"]["alpha"]
+    beta = config["galaxy"]["rotation"]["beta"]
+    gamma = config["galaxy"]["rotation"]["gamma"]
+    logger = get_logger()
+
     def rotate_galaxy(inputs: dict[str, jax.Array], type: str = "face-on"):
-        # TODO: I guess this breaks the functional programming paradigm?
-        print("rotating galaxy: ", type)
+        logger.info(f"Rotating galaxy with alpha={alpha}, beta={beta}, gamma={gamma}")
+
+        # Get the inputs
         coords = inputs["coords"]
         velocities = inputs["velocities"]
+        masses = inputs["mass"]
+        halfmass_radius = inputs["halfmassrad_stars"]
+
+        # Rotate the galaxy
+        coords, velocities = rotate_galaxy_core(
+            positions=coords,
+            velocities=velocities,
+            masses=masses,
+            halfmass_radius=halfmass_radius,
+            alpha=alpha,
+            beta=beta,
+            gamma=gamma,
+        )
+
+        # Update the inputs
         inputs["coords"] = coords
         inputs["velocities"] = velocities
         return inputs
