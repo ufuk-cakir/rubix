bdf1457491fd884be0e63062771a729877d6d089
Tobias Buck <TobiBu@users.noreply.github.com>
2024-06-15 20:36:59 +0200

    [DRAFT] 60 add support for fsps stellar templates (#63)
    * refactored SSPGrid class to have one base class, implemented HDF% class and a class for the pyPipe3D SSP model format. Added a checkout_SSP_template function to automatically download SSP models from a url if not present on disc. Added pyPipe3D template for Mastar_SLOG model.
    
    * small changes to units and mass-to-light-ratio in template
    
    * changed class inheritance
    
    * removed mass-to-light from config
    
    * added wget as dependence, fixed some f string issues.
    
    * removed dependence on wget in favour of requests for the file download
    
    * added tests for new SSP models and subclasses
    
    * remove import dict
    
    * fix class method call from_file in sub-classes
    
    * fixed all tests and added a logger
    
    * reshape flux array in pypipe3d class, check for trailing / in source, updated test accordingly.
    
    * remove unnecessary import.
    
    * move float64 to float32 and add class dictionary
    
    * first attempt to include support for fsps. some tests are still failing.
    
    * try to fix test...
    
    * fix typo in test
    
    * modified test
    
    * fixed unittest
    
    * * fix the unit tests for fsps ssp templates,
    
    * add function to write fsps templates to disk.
    
    * still needs some thinking how to load pre-existing fsps templates from file
    
    * fixed ssp_template notebook to comply with new SSPGrid class.
    
    * fixed test mimicking fsps is not installed, added pre-commit hooks to check en of file amnd trailing whitespaces and check yaml files.
    
    * changed the fsps implementation for more control
    
    * when using fsps the rubix_config.yaml now uses the source entry to either rerun_from_scratch the fsps template or load_from_file an existing template
    
    * newly calculated fsps templates are silently stored under filename
    
    * relevant parameters are now passed all the way to pyfsps
    
    * tests are adapted accordingly
    
    * notebook with examples added
    
    * change name into template
    
    * exclude fsps related cells from testing
    
    * change check for fsps to use importlib instead of try except
    
    * exclude further cells, so we have no matplotlib dependence
    
    ---------
    
    Co-authored-by: Tobias Buck <buck@mpia.de>

--- tests/test_ssp_templates.py
+++ tests/test_ssp_templates.py
@@ -10,3 +10,13 @@ def test_bruzual_charlot():
     assert isinstance(ssp.flux, jnp.ndarray)
     assert isinstance(ssp.age, jnp.ndarray)
     assert isinstance(ssp.metallicity, jnp.ndarray)
+#
+#def test_pyPipe3D():
+#    from rubix.spectra.ssp.templates import MaStar_CB19_SLOG_1_5 as ssp
+#
+#    print(ssp)
+#    assert ssp.__class__.__name__ == "Mastar Charlot & Bruzual (2019)"
+#    assert isinstance(ssp.wavelength, jnp.ndarray)
+#    assert isinstance(ssp.flux, jnp.ndarray)
+#    assert isinstance(ssp.age, jnp.ndarray)
+#    assert isinstance(ssp.metallicity, jnp.ndarray)
