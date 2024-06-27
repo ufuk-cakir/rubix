57b08a4901458ce99a16b6150228330f2d0e7799
Tobias Buck <buck@mpia.de>
2024-06-28 00:32:10 +0200

    fix requests SSLError via second try with verify=False, adapted the tests accordingly

--- tests/test_ssp_grid.py
+++ tests/test_ssp_grid.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from unittest.mock import patch, MagicMock
 from rubix.spectra.ssp.grid import SSPGrid, HDF5SSPGrid, pyPipe3DSSPGrid
+import rubix
 import numpy as np
 import jax.numpy as jnp
 import os
@@ -485,6 +486,42 @@ def test_checkout_SSP_template_file_download_error_HDF5SSPGrid():
                 str(e)
                 == "Could not download file test.hdf5 from url http://example.com/."
             )
+
+
+def test_checkout_SSP_template_SSL_error_HDF5SSPGrid():
+    config = {
+        "format": "hdf5",
+        "file_name": "test.hdf5",
+        "source": "http://example.com/",  # This URL will raise an exception when accessed
+        "fields": {
+            "age": {"name": "age", "in_log": False, "units": "Gyr"},
+            "metallicity": {"name": "metallicity", "in_log": False, "units": ""},
+            "wavelength": {"name": "wavelength", "in_log": False, "units": "Angstrom"},
+            "flux": {"name": "flux", "in_log": False, "units": "Lsun/Angstrom"},
+        },
+        "name": "TestSSPGrid",
+    }
+    file_location = "/path/to/files"
+
+    # Mock the requests.get function to raise an exception
+    with patch("requests.get") as mock_get:
+        mock_get.side_effect = [
+            requests.exceptions.SSLError("Download error"),
+            requests.exceptions.HTTPError("Download error"),
+        ]
+
+        # Call the function and verify that it raises a ValueError
+        try:
+            HDF5SSPGrid.checkout_SSP_template(config, file_location)
+            assert False, "Expected ValueError to be raised"
+        except FileNotFoundError as e:
+            assert (
+                str(e)
+                == "Could not download file test.hdf5 from url http://example.com/."
+            )
+        assert rubix.spectra.ssp.grid.requests.get.called_once_with(
+            config["source"] + "/" + config["file_name"], verify=False
+        )
 
 
 def test_checkout_SSP_template_file_download_failed():
