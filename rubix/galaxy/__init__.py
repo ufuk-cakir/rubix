f5a30a0fabb144b5c4a8b0129b0b573e44aac07f
nihatog <nihat.oguz@web.de>
2024-11-13 13:45:36 +0100

    Include NihaoHandler

--- rubix/galaxy/__init__.py
+++ rubix/galaxy/__init__.py
@@ -1,1 +1,1 @@
-from .input_handler import IllustrisHandler, BaseHandler, IllustrisAPI, get_input_handler
+from .input_handler import IllustrisHandler, NihaoHandler, BaseHandler, IllustrisAPI, get_input_handler
