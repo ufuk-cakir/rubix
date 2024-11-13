1e233c4a7b98ab1844b92ac8d20e6d011dd7b7cf
nihatog <nihat.oguz@web.de>
2024-11-13 13:44:58 +0100

    Include NihaoHandler

--- rubix/galaxy/input_handler/__init__.py
+++ rubix/galaxy/input_handler/__init__.py
@@ -2,6 +2,7 @@ from .illustris import IllustrisHandler
 from .base import BaseHandler
 from .api.illustris_api import IllustrisAPI
 from .factory import get_input_handler
+from .nihao import NihaoHandler
 
 
-__all__ = ["IllustrisHandler", "BaseHandler", "IllustrisAPI", "get_input_handler"]
+__all__ = ["IllustrisHandler", "BaseHandler", "IllustrisAPI", "get_input_handler", "NihaoHandler"]
