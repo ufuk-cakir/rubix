e8f4c5fca6d940fa246b1e387c722431e49cfa22
nihatog <nihat.oguz@web.de>
2024-12-16 14:35:52 +0100

    Added logging instead of printing

--- rubix/galaxy/input_handler/factory.py
+++ rubix/galaxy/input_handler/factory.py
@@ -4,14 +4,20 @@ from .base import BaseHandler
 
 def get_input_handler(config: dict, logger=None) -> BaseHandler:
     """Creates a handler based on the config"""
+    if logger is None:
+        import logging
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+
     simulation_name = config["simulation"]["name"]
-    print(f"Debug: Simulation name received = {simulation_name}")
+    logger.info(f"Simulation name received: {simulation_name}")
     
     if simulation_name == "IllustrisTNG":
-        print("Using IllustrisHandler")  # debug
+        logger.info("Using IllustrisHandler")
         return IllustrisHandler(**config["simulation"]["args"], logger=logger)
     elif simulation_name == "NIHAO":
-        print("Using NihaoHandler")  # debug
+        logger.info("Using NihaoHandler")
         return NihaoHandler(**config["simulation"]["args"], logger=logger)
     else:
+        logger.error(f"Simulation {config['simulation']} is not supported")
         raise ValueError(f"Simulation {config['simulation']} is not supported")
