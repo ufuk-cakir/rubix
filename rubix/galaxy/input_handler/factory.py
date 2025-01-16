052e900fe89cdcf2f0b4c9b55750b900ae6ca0a7
anschaible <annalena.schaible@iwr.uni-heidelberg.de>
2025-01-16 15:31:26 +0100

    copy nihao handler

--- rubix/galaxy/input_handler/factory.py
+++ rubix/galaxy/input_handler/factory.py
@@ -1,5 +1,6 @@
 from .base import BaseHandler
 from .illustris import IllustrisHandler
+from .pynbody import PynbodyHandler
 from typing import Union
 from unittest.mock import MagicMock
 from jaxtyping import Array, Float, jaxtyped
@@ -22,5 +23,11 @@ def get_input_handler(config: dict, logger=None) -> Union[BaseHandler, MagicMock
     """
     if config["simulation"]["name"] == "IllustrisTNG":
         return IllustrisHandler(**config["simulation"]["args"], logger=logger)
+    elif config["simulation"]["name"] == "NIHAO":
+        logger.info("Using PynbodyHandler to load a NIHAO galaxy")
+        simulation_args = config["simulation"]["args"]
+        if "galaxy" in config and "dist_z" in config["galaxy"]:
+            simulation_args["dist_z"] = config["galaxy"]["dist_z"]
+        return PynbodyHandler(**simulation_args, logger=logger)
     else:
         raise ValueError(f"Simulation {config['simulation']} is not supported")
