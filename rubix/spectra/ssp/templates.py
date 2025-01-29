b66d864581c8df34451e02affdfb376ed766b564
anschaible <annalena.schaible@iwr.uni-heidelberg.de>
2025-01-29 16:10:12 +0100

    compare stellar libaries

--- rubix/spectra/ssp/templates.py
+++ rubix/spectra/ssp/templates.py
@@ -15,4 +15,6 @@ BruzualCharlot2003 = get_ssp_template("BruzualCharlot2003")
 # having this here forces a dwonload of the Mastar data
 # MaStar_CB19_SLOG_1_5 = get_ssp_template("Mastar_CB19_SLOG_1_5")
 
-__all__ = ["BruzualCharlot2003"]  # , "Mastar_CB19_SLOG_1_5"]
+# FSPS = get_ssp_template("FSPS")
+
+__all__ = ["BruzualCharlot2003"]  # , "FSPS"]# , "Mastar_CB19_SLOG_1_5"]
